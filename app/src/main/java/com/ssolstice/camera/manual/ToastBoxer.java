72470607669f0a9e6350ab4109e0817f0f5a7431
Phung Nguyen <nhphung216@gmail.com>
2025-08-23 13:32:02 +0700

    update package name

---
+++ app/src/main/java/com/ssolstice/camera/manual/ToastBoxer.java
@@ -0,0 +1,12 @@
+package com.ssolstice.camera.manual;
+
+import android.widget.Toast;
+
+/** Allows methods to update a Toast with a new Toast.
+ */
+public class ToastBoxer {
+    public Toast toast;
+
+    public ToastBoxer() {
+    }
+}
